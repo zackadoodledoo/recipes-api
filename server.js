ae57801d189bb99403efe367a5718b4be2dff95f
zackadoodledoo <sin12014@byui.edu>
2026-02-06 19:35:42 -0800

    get rid of not necessary code

--- server.js
+++ server.js
@@ -17,13 +17,5 @@ await client.connect();
 const db = client.db("recipesDB"); // new database
 const recipes = db.collection("recipes");
 
-// Simple test route for Swagger practice
-app.get("/", (req, res) => {
-  const name = req.query.name || "Guest";
-  res.send(`Hello, ${name}!`);
-});
-
-// Swagger docs
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
